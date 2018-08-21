f6642eb96dc9ef47084520163e84f2f246351cdd
159719 <159719@192.168.56.1>
2018-08-21 10:50:35 +0530

    Added additional info

--- myspringjwt/src/main/java/com/example/jwt/App.java
+++ myspringjwt/src/main/java/com/example/jwt/App.java
@@ -1,15 +1,32 @@
 package com.example.jwt;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class App 
-{
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+public class App {
+	private static Logger log = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) throws UnknownHostException {
+		Environment env = SpringApplication.run(App.class, args).getEnvironment();
+
+		String protocol = "http";
+		if (env.getProperty("server.ssl.key-store") != null) {
+			protocol = "https";
+		}
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\t{}://localhost:{}\n\t"
+						+ "External: \t{}://{}:{}\n\t"
+						+ "Profile(s): \t{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), protocol, env.getProperty("server.port"), protocol,
+				InetAddress.getLocalHost().getHostAddress(), env.getProperty("server.port"), env.getActiveProfiles());
 	}
-	
 
 }
